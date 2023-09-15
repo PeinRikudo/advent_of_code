7ff0bf7cf1cdb316bb6f0e3e76fcaf8337412501
PeinRikudo <gauthier.pandele@outlook.com>
2023-09-15 16:17:29 +0200

    Day 1 part 1 is completed

--- 2022/day1/src/main.rs
+++ 2022/day1/src/main.rs
@@ -3,8 +3,21 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let content = read_file_from_args(&args);
+
+    let mut max_calories = content
+        .split("\r\n\r\n")
+        .map(|e| e.lines().map(|e| e.parse::<i32>().unwrap()).sum())
+        .collect::<Vec<i32>>();
+
+    max_calories.sort_unstable();
+
+    println!("{:?}", max_calories.into_iter().rev().take(1).sum::<i32>());
+}
+
+fn read_file_from_args(args: &[String]) -> String {
     let file_path = &args[1];
     let content = fs::read_to_string(file_path).unwrap();
-    println!("The file: {}", file_path);
-    println!("Contain: {}", content);
+
+    content
 }
